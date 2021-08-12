fe0d79cfb49bdd242138f70115c6f7f0f1826e1c
Whyimams <88605576+Whyimams@users.noreply.github.com>
2021-08-12 18:08:20 +0300

    Add files via upload

---
+++ inputlarla çalışlmak basit workshop.py
@@ -0,0 +1,13 @@
+#workshop 1 x ve y'yebir sayı ata ve bunları değiştir
+x=10
+y=20
+print(x,y)
+x,y=y,x
+print(x,y)
+
+#mile'i kilometreye çevirme basit matematik hesaplamaları 
+donüsümorani = 1.65
+km= int(input("kaç kilometre yol aldınız?"))
+mile=km*donüsümorani
+print(str(km)+"km ="+ str(mile)+" mile eder")
+
