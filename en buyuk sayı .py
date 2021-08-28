d5b870ab6c5e8127441eae5a80a963f389bce7ce
Whyimams <88605576+Whyimams@users.noreply.github.com>
2021-08-29 00:12:39 +0300

    Add files via upload

---
+++ en buyuk sayı .py
@@ -0,0 +1,9 @@
+a=int(input(" sayı giriniz"))
+b=int(input(" sayı giriniz"))
+c=int(input(" sayı giriniz"))
+if a>=b and a>c:
+    print("en büyük sayı a dır")
+elif b>=a and b>=c:
+    print("en buyuk b dir")
+else :
+    print("en büyük c dir ")
---
+++ pozitif mi negatif mi .py
@@ -0,0 +1,8 @@
+a=int(input("sayı giriniz"))
+if a>0 :
+    print("pozitif")
+elif a==0:
+    print("a sıfıra eşittir")
+else :
+    print(" a negatif")
+    
