dd560110d5860121b4abfa309fb87d625856f41f
TheImam <88605576+Whyimams@users.noreply.github.com>
2022-11-08 15:10:48 +0300

    Add files via upload

---
+++ madlib taslak.py
@@ -0,0 +1,7 @@
+adj=input("adjective: ")
+verb1=input("Verb: ")
+verb2=input("Verb: ")
+kiz_ismi = input( "Kız ismi: ")
+madlib=f"text {adj}! text {verb1} text{verb2}! text {kiz_ismi} "
+
+print( madlib)
